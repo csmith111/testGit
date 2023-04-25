3a751264f0288401816c44a0693abb93df68c0de
C Smith <csmith@gmail.com>
2023-04-25 10:26:36 -0400

    test commit - added function power(x, n)

--- BasicMath.py
+++ BasicMath.py
@@ -1,9 +1,11 @@
-def sqr(n):
-    return n*n #updated
+def sqr(x:float):
+    return x**2 #updated
 
+def cube(x:float):
+    return x**3 #updated on master
 
-def cube(n):
-    return n*3 #updated on master
+def power(x:float, n:float):
+    return x**n #updated on master
 
 
 
